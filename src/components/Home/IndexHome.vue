b3871dcea45907a773aab6a240e78e1d77a6dfed
eriks junior <eriksjunior13@gmail.com>
2022-05-27 02:11:21 -0300

    commit

--- src/components/Home/IndexHome.vue
+++ src/components/Home/IndexHome.vue
@@ -1,22 +1,24 @@
 <template>
-  <div
-    class="h-screen flex justify-center"
-    style="padding: 0; margin: 0; font-size: 50px; width: 100%"
-  >
-    <div class="w-10/12">
-      <div
-        id="containerGeneral"
-        class="my-5 mt-3 text-ellipsis grid grid-cols-4 gap-5"
-        ref="containerGeneral"
-      >
-        <div v-for="item in arrayTeste" :key="item">
-          <CardItem />
+  <div class="">
+    <div
+      class="h-auto flex justify-center"
+      style="padding: 0; margin: 0; font-size: 50px; width: 100%"
+    >
+      <div class="w-10/12">
+        <div
+          id="containerGeneral"
+          class="my-5 mt-3 text-ellipsis grid grid-cols-4 gap-5"
+          ref="containerGeneral"
+        >
+          <div v-for="item in arrayTeste" :key="item">
+            <CardItem />
+          </div>
         </div>
       </div>
-      <button
-        style="background-color: white; border-radius: 10px; width: 200px"
-        @click="addNewDiv"
-      >
+    </div>
+
+    <div class="buttonAdd">
+      <button class="bg-slate-100 w-32 rounded-md" @click="addNewDiv">
         ADD
       </button>
     </div>
@@ -94,7 +96,13 @@ export default defineComponent({
   border-bottom-right-radius: 20px;
 }
 #containerGeneral {
-  border: solid 2px red;
+  /* border: solid 2px red; */
   height: auto;
+}
+.buttonAdd {
+  z-index: 1;
+  position: fixed;
+  left: 80%;
+  top: 90%;
 }
 </style>
